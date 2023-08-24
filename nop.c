c6968fb7629b50cd9748dbca9428a16eb3ba2d28
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-08-24 21:16:56 +0100

    updated nop

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *c_nop - to do nothing
+  *@head: pointer to the head of the stack
+  *@count: line umber
+  *Return: void;
+ */
+void c_nop(stack_t **head, unsigned int count)
+{
+	(void) count;
+	(void) head;
+}
