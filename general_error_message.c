220e2442778ec9e883c70695b0aa1e87cba0dd5b
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-08-24 23:21:42 +0100

    correct betty

--- general_error_message.c
+++ general_error_message.c
@@ -1,5 +1,13 @@
 #include "monty.h"
 
+/**
+ * message_error - to print error message
+ * @head: pointer to the header
+ * @string: string to be printed
+ * @count: line number
+ * Return: void
+ */
+
 void message_error(stack_t **head, char *string, int count)
 {
 	fprintf(stderr, "L%d: %s\n", count, string);
